1e558177916e645a5217a168da45f6cb8c414840
Rey Barcelo <rey@officetogether.com>
2022-06-24 15:48:17 -0400

    some html

---
+++ serenity/src/Serenity.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+export function Serenity(){
+    return (
+        <>
+            <div>What problem has arisen?</div>
+            <TextField />
+        </>
+    );
+}
